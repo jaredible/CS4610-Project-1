ce840aeb66007003b542eddfe2ca86e863958744
Jared Diehl <jareddiehl96@gmail.com>
2020-01-28 18:28:15 -0600

    Basic setup

--- index.php
+++ index.php
@@ -5,9 +5,16 @@ require_once "config/database.php";
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Testing</title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.2/dist/semantic.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/styles.css">
     </head>
     <body>
         Test
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.2/dist/semantic.min.js"></script>
+        <script src="/js/main.js"></script>
     </body>
 </html>
