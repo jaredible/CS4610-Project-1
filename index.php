3f92fc2e04063745b53632f5f0e41aa7d7cedcae
Jared Diehl <jareddiehl96@gmail.com>
2020-01-28 18:18:58 -0600

    Linking database configurations

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once "config/database.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Testing</title>
+    </head>
+    <body>
+        Test
+    </body>
+</html>
