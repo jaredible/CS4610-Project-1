1365f1a2d2c70a3a9024b148a7b60dc976dff256
Jared Diehl <jareddiehl96@gmail.com>
2020-02-03 23:06:19 -0600

    Updated

--- project1/index.php
+++ project1/index.php
@@ -1,24 +1,54 @@
 <?php
-require_once "config/database.php";
+session_start();
 
-if (isset($_SESSION["isAdmin"])) {
-} else {
+//$_SESSION["user_id"] = 69;
+
+define("DB_SERVER", "192.168.64.3");
+define("DB_USERNAME", "admin");
+define("DB_PASSWORD", "");
+define("DB_NAME", "university");
+define("ADMIN_PASSWORD_HASH", "");
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($conn === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$password = "password";
-$hash = password_hash($password, PASSWORD_BCRYPT, [
-    "cost" => 12
-]);
+//if (isset($_SESSION["isAdmin"])) {
+//} else {
+//}
+
+//$password = "password";
+//$hash = password_hash($password, PASSWORD_BCRYPT, [
+//    "cost" => 12
+//]);
 
 //echo $hash;
-if (password_verify($password, $hash)) {
+//if (password_verify($password, $hash)) {
     //echo "Yes";
-} else {
+//} else {
     //echo "No";
+//}
+
+$sql = "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'university'";
+$result = mysqli_query($conn, $sql);
+if ($result->num_rows > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        $table_names[] = $row[0];
+    }
+}
+
+$sql = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'university' AND TABLE_NAME = 'course'";
+$result = mysqli_query($conn, $sql);
+if ($result->num_rows > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row[0] . "<br>";
+    }
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
@@ -31,33 +61,10 @@ if (password_verify($password, $hash)) {
     </head>
     <body>
         <div class="ui secondary stackable menu">
-            <div class="item">
-                <img src="img/logo.png">
-            </div>
-            <div class="right menu">
-                <div class="item">
-                    <div class="ui basic button">
-                        <i class="shield icon"></i> View as Administrator
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="main ui container">
-            <div class="ui form">
-                <div class="two fields">
-                    <div class="field">
-                        <label>Database</label>
-                        <div class="ui selection dropdown">
-                            <input type="hidden" name="database" value="<?php if (isset($_GET['database'])) { echo $_GET['database']; } ?>" onchange="onChangeDatabase()">
-                            <i class="dropdown icon"></i>
-                            <div class="default text">Select one...</div>
-                            <div class="menu">
-                                <div class="item" data-value="test1">Test1</div>
-                                <div class="item" data-value="test2">Test2</div>
-                                <div class="item" data-value="test3">Test3</div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="ui segment">
+                <div class="ui form">
                     <div class="field">
                         <label>Table</label>
                         <div class="ui selection dropdown">
@@ -73,131 +80,90 @@ if (password_verify($password, $hash)) {
                     </div>
                 </div>
             </div>
-            <div class="ui divider"></div>
-            <div class="ui stackable grid">
-                <div class="row">
-                    <div class="eight wide column">
-                        <div class="ui floating labeled icon dropdown button">
-                            <i class="plus icon"></i>
-                            <span class="text">New</span>
-                            <div class="menu">
-                                <div class="item">Record</div>
-                                <div class="disabled item">Attribute</div>
+            <div class="ui segment">
+                <div class="ui stackable grid">
+                    <div class="row">
+                        <div class="eight wide column">
+                            <div class="ui basic buttons">
+                                <button class="ui button">New</button>
+                            </div>
+                        </div>
+                        <div class="right aligned eight wide column">
+                            Sort
+                            <div class="ui selection dropdown" style="min-width: 0;">
+                                <input type="hidden" name="column" value="1">
+                                <i class="dropdown icon"></i>
+                                <div class="text">Column1</div>
+                                <div class="menu">
+                                    <div class="item" data-value="1">Column1</div>
+                                    <div class="item" data-value="2">Column2</div>
+                                    <div class="item" data-value="3">Column3</div>
+                                    <div class="item" data-value="4">Column4</div>
+                                    <div class="item" data-value="5">Column5</div>
+                                </div>
+                            </div>
+                            by
+                            <div class="ui selection dropdown" style="min-width: 0;">
+                                <input type="hidden" name="column" value="asc">
+                                <i class="dropdown icon"></i>
+                                <div class="text">Ascending</div>
+                                <div class="menu">
+                                    <div class="item" data-value="asc">Ascending</div>
+                                    <div class="item" data-value="desc">Descending</div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="right aligned eight wide column">
-                        Sort
-                        <div class="ui selection dropdown" style="min-width: 0;">
-                            <input type="hidden" name="column" value="1">
-                            <i class="dropdown icon"></i>
-                            <div class="text">Column1</div>
-                            <div class="menu">
-                                <div class="item" data-value="1">Column1</div>
-                                <div class="item" data-value="2">Column2</div>
-                                <div class="item" data-value="3">Column3</div>
-                                <div class="item" data-value="4">Column4</div>
-                                <div class="item" data-value="5">Column5</div>
-                            </div>
-                        </div>
-                        by
-                        <div class="ui selection dropdown" style="min-width: 0;">
-                            <input type="hidden" name="column" value="asc">
-                            <i class="dropdown icon"></i>
-                            <div class="text">Ascending</div>
-                            <div class="menu">
-                                <div class="item" data-value="asc">Ascending</div>
-                                <div class="item" data-value="desc">Descending</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row" style="padding: 0;">
-                    <div class="sixteen wide column">
-                        <table class="ui single line table center aligned">
-                            <thead class="full-width">
-                                <tr>
-                                    <th>Column1</th>
-                                    <th>Column2</th>
-                                    <th>Column3</th>
-                                    <th>Column4</th>
-                                    <th>Column5</th>
-                                    <th class="collapsing">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                for ($i = 0; $i < 5; $i++) {
-                                ?>
-                                <tr>
+                    <div class="row">
+                        <div class="sixteen wide column">
+                            <table class="ui single line table center aligned">
+                                <thead class="full-width">
+                                    <tr>
+                                        <th>Column1</th>
+                                        <th>Column2</th>
+                                        <th>Column3</th>
+                                        <th>Column4</th>
+                                        <th>Column5</th>
+                                        <th class="collapsing">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <?php
-                                    for ($j = 0; $j < 5; $j++) {
+                                    for ($i = 0; $i < 5; $i++) {
                                     ?>
-                                    <td>
+                                    <tr>
                                         <?php
-                                        if ($i == 0) {
+                                        for ($j = 0; $j < 5; $j++) {
                                         ?>
-                                        <div class="ui fluid input">
-                                            <input type="text" placeholder="Data...">
-                                        </div>
-                                        <?php
-                                        } else {
-                                        ?>
-                                        Data<?php echo $i + 2 ?>
+                                        <td>
+                                            Data<?php echo $i + 2 ?>
+                                        </td>
                                         <?php
                                         }
                                         ?>
-                                    </td>
+                                        <td>
+                                            <div class="ui basic buttons">
+                                                <button class="ui button disabled">Edit</button>
+                                                <button class="ui button disabled">Delete</button>
+                                            </div>
+                                        </td>
+                                    </tr>
                                     <?php
                                     }
                                     ?>
-                                    <td>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button">Edit</button>
-                                            <button class="ui button">Delete</button>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <?php
-                                }
-                                ?>
-                            </tbody>
-                            <tfoot class="full-width">
-                                <tr>
-                                    <th>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button"><i class="fitted arrow up icon"></i></button>
-                                            <button class="ui button"><i class="fitted arrow down icon"></i></button>
-                                        </div>
-                                    </th>
-                                    <th>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button"><i class="fitted arrow up icon"></i></button>
-                                            <button class="ui button"><i class="fitted arrow down icon"></i></button>
-                                        </div>
-                                    </th>
-                                    <th>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button"><i class="fitted arrow up icon"></i></button>
-                                            <button class="ui button"><i class="fitted arrow down icon"></i></button>
-                                        </div>
-                                    </th>
-                                    <th>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button"><i class="fitted arrow up icon"></i></button>
-                                            <button class="ui button"><i class="fitted arrow down icon"></i></button>
-                                        </div>
-                                    </th>
-                                    <th>
-                                        <div class="ui basic buttons">
-                                            <button class="ui button"><i class="fitted arrow up icon"></i></button>
-                                            <button class="ui button"><i class="fitted arrow down icon"></i></button>
-                                        </div>
-                                    </th>
-                                    <th></th>
-                                </tr>
-                            </tfoot>
-                        </table>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td>Column1</td>
+                                        <td>Column2</td>
+                                        <td>Column3</td>
+                                        <td>Column4</td>
+                                        <td>Column5</td>
+                                        <td>Action</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -209,3 +175,6 @@ if (password_verify($password, $hash)) {
         <script src="js/main.js"></script>
     </body>
 </html>
+<?php
+mysqli_close($conn);
+?>
